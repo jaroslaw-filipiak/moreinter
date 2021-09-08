12306c33d43a3f99a3124427e99388405a45ed94
jaroslaw-filipiak <info@j-filipiak.pl>
2021-09-08 10:04:20 +0200

    purge tailwind for portfolio page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,45 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [
+    "./index.html",
+    "./portfolio.html",
+    "./*.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['Gotham', 'system-ui',],
-      'book': ['Gotham Book', 'system-ui',],
+      sans: ["Gotham", "system-ui"],
+      book: ["Gotham Book", "system-ui"],
     },
     fontSize: {
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'tiny': '.938rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.5rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
-     },
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".938rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.5rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
     colors: {
-      'main-blue': '#497EC0',
-      'gray-7070': '#707070',
-      'gray-f8': '#f8f8f8',
-      'black': '#000',
-      'white': '#fff',
+      "main-blue": "#497EC0",
+      "gray-7070": "#707070",
+      "gray-f8": "#f8f8f8",
+      black: "#000",
+      white: "#fff",
     },
     extend: {
       screens: {
-        '3xl': '1900px',
-      }
+        "3xl": "1900px",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
